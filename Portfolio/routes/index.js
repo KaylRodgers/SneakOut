b5df33e481969e18865750c397f8e3c53f722028
Kayl Rodgers <kayleigh-rodgers@outlook.com>
2023-09-28 01:22:17 -0400

    updated commit of previous (was in wrong directory when commit was made) v1.1

--- Portfolio/routes/index.js
+++ Portfolio/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Define routes for different pages
+// Define routes for different pages, renders and passes data as needed
 router.get('/', (req, res) => {
     res.render('index', { pageTitle: 'Home' });
 });
@@ -9,6 +9,19 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
     res.render('about', { pageTitle: 'About' });
 });
+
+router.get('/contact', (req, res) => {
+    res.render('contact', { pageTitle: 'Contact' });
+});
+
+router.get('/projects', (req, res) => {
+    res.render('projects', { pageTitle: 'Projects' });
+});
+
+router.get('/services', (req, res) => {
+    res.render('services', { pageTitle: 'Services' });
+});
+
 
 // Define routes for other pages
 
