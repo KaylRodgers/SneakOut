e55e936a6951a785d3fb1557f5edac787e195dc7
tristanmbugua <128255762+tristanmbugua@users.noreply.github.com>
2023-11-22 23:06:49 -0500

    Reconfigured folder structure.
    I did this so my group members would have an easier time understanding the code written. Functionality is still intact.

--- server.js
+++ server.js
@@ -1,180 +1,32 @@
-import config from "./config/config.js";
-import User from './models/users.js';
-import app from "./express.js";
-import crypto from "crypto";
-import jwt from "jsonwebtoken";
-import { expressjwt } from "express-jwt";
+const config = require("./config/config.js");
+const app = require("./server/express.js");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const path = require("path");
 
-const authSignIn = 
-async (req, res, next) => {
-  try {
-    const email = String(req.body.email);
-    const user = (await User.find({ email: email }))[0];
-    if (!user) {
-      return res.status().json({error: "User not found"});
-    }
-    if (!(
-          crypto
-            .createHmac('sha256',user.salt)
-            .update(req.body.password)
-            .digest('hex')
-                    ===
-          user.hashed_password
-          
-        )) {
-      return res.status().send({error: "Email and password don't match."});
-    }
-    const token = jwt.sign({ _id: user._id }, config.jwtSecret);
-    res.cookie('t', token, { expire: new Date() + 9999 });
-    config.userToken = token;
-    return res.json({
-      token,
-      user: {
-        _id: user._id,
-        name: user.name,
-        email: user.email
-      }
-    })
-    } catch (err) {
-      console.log(err.message);
-      return res.status("401").json({error: "Could not sign in"});
-    }
-}
-;
+app.get('/', (req, res) => {
+  res.json({message: "Welcome to User applicaiton!"});
+})
 
-const authSignOut = 
-  (req, res, next) => {
-    res.clearCookie("t"); 
-    return res.status(200).json({message: "signed out"});
-  }
-;
+/*
+// app.engine('html', require('js').renderFile);
+app.set('view engine', 'js');
 
-const hasAuthorization =
-  async (req, res, next) => {
-    const authorized = 
-        config.userToken == req.cookies.t
-        ;
-    if (!(authorized)) {
-      return res.status(401).json({
-        error: "User is not authorized"
-      });
-    }
-    next();
-  }
-;
+app.get('/signupreact', (req, res) => {
+    const fileName = path.join(__dirname, "./views/signup.js");
+    res.render(fileName);
+});
 
-
-app
-  .route('/')
-      .all(
-              (req, res) => {
-                  res.json({message: "Welcome to SneakOut!"});
-              }
-          );
-
-app
-  .route('/auth/signin')
-  .post(
-    authSignIn
-    );
-
-app
-  .route('/auth/signout')
-  .get(
-    authSignOut 
-  );
-
-app
-  .route("/auth/signup")
-  .post(
-    async (req, res) => {
-      const user = new User(req.body);
-      try {
-        await user.save();
-        return res.status(200).json({
-          message: "Successfully created!",
-        });
-      } catch (err) {
-        return res.status(400).json({
-          message: "user could not be created!"
-        });
-      }
-    }
-  )
-
-app
-    .route("/users")
-        //Completed
-        .get(
-              (hasAuthorization)
-              ,
-              (
-                async (req, res) => {
-                  const user = await User.find({ email: req.body.email });
-                  return res.json(
-                    (user)[0]
-                    );
-                }
-              )
-        )
-        //Completed. This updates a document using the username value.
-        .put(
-            (hasAuthorization),
-            (
-              async (req, res) => {
-                try {
-                //   let user = req.profile;
-                  const user = new User(req.body);
-                //   console.log(user);
-                  await User.updateOne({username: req.body.username}, req.body);
-                  if (User.find(req.body))
-                  {
-                    res.json({"message": "Update successful!"});
-                  } else {
-                    throw err;
-                  }
-                  
-                } catch (err) {
-                  return res.status(401).json({
-                    message: "No user has been updated!"
-                  });
-                }
-              }
-            )
-        )
-        //Completed.
-        .delete(
-          (hasAuthorization),
-          (
-            async (req, res) => {
-              try {
-                let user = req.body.email;
-                let deletedUser = await User.deleteOne({email: user});
-                let isUserDeleted = await User.find({email: user});
-                if (isUserDeleted) {
-                  return res.status(200).json({
-                    message: "User is still active. Deletion unsuccessful.",
-                  });
-                } else {
-                  return res.status(200).json({
-                    deletedUser,
-                    message: "Successfully deleted!",
-                  });
-                }
-              } catch (err) {
-                console.log(err)
-                return res.status(400).json({
-                  message: "user could not be deleted!"
-                });
-              }
-            }
-          )
-        );
-
+app.get('/signinreact', (req, res) => {
+  const fileName = path.join(__dirname, "./views/signin.ejs");
+  res.render(fileName);
+});
+*/
 
 app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.info("Server started on port %s.", config.port)
+  if (err) {
+      console.log(err);
+  }
+  console.info("Server started on port %s.", config.port)
 });
