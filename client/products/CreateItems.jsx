96c5a4d30814c21135c0b90fbf19acfeda485bc2
tristanmbugua <128255762+tristanmbugua@users.noreply.github.com>
2023-12-05 18:05:44 -0500

    Create item functionality complete.

--- client/products/CreateItems.jsx
+++ client/products/CreateItems.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useState } from 'react';
-import products from './api-products.js'
 import {
     Card,
     CardContent,
@@ -9,40 +8,68 @@ import {
     CardActions,
     Icon,
     Button,
-  } from "@material-ui/core";
+} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core';
+import products from './api-products.js'
 
 
 
 const CreateItems = () => {
     const [values, setValues] = useState({
-        
+        link: "",
+        colourway: "",
+        model: "",
+        price: 0
+
     })
 
+    const handleChange = username => event => {
+        setValues({ ...values, [username]: event.target.value })
+    }
+
+    //Complete
+    const clickSubmit = () => {
+        const sneaker = {
+            link: values.link || undefined,
+            colourway: values.colourway || undefined,
+            model: values.model || undefined,
+            price: values.price || undefined
+        }
+
+        products.create(sneaker).then((data) => {
+            if (data.error) {
+                console.log(data.error);
+                setValues({ ...values, error: data.error })
+
+            } else {
+                setValues({ ...values, error: '', redirectToReferrer: true })
+            }
+        })
+    }
 
     return (
         <Card >
             <CardContent>
-              <Typography variant="h5">
+                <Typography variant="h5">
                     Edit Sneaker
-              </Typography>
-              
-              {/* <TextField id="email" type="username" label="Username" value={values.username} onChange={handleChange('username')} margin="normal" /><br />
-      
-              <TextField id="email" type="email" label="Email" value={values.email} onChange={handleChange('email')} margin="normal" /><br />
-              
-              <TextField id="password" type="password" label="Password" value={values.password} onChange={handleChange('password')} margin="normal" /> */}
-              {/* <br /> {
-                values.error && (<Typography component="p" color="error">
-                  <Icon color="error" className={classes.error}>error</Icon>
-                  {values.error}
-                </Typography>)
-              } */}
+                </Typography>
+
+                <TextField id="link" label="Image Link" value={values.link} onChange={handleChange('link')} margin="normal" /><br />
+                <TextField id="colourway" label="Colourway" value={values.colourway} onChange={handleChange('colourway')} margin="normal" /><br />
+                <TextField id="model" label="Model" value={values.model} onChange={handleChange('model')} margin="normal" /><br />
+                <TextField id="price" label="Price" value={values.price} onChange={handleChange('price')} margin="normal" /><br />
+
+                <br /> {
+                    values.error && (<Typography component="p" color="error">
+                        <Icon color="error" className={classes.error}>error</Icon>
+                        {values.error}
+                    </Typography>)
+                }
             </CardContent>
             <CardActions>
-              {/* <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Sign In</Button> */}
+                <Button color="primary" variant="contained" onClick={clickSubmit}>Create Sneaker</Button>
             </CardActions>
-          </Card>
-        );
+        </Card>
+    );
 };
 export default CreateItems;
