67260ee78115ca039ccc3c36955c43203d6c6ce0
tristanmbugua <128255762+tristanmbugua@users.noreply.github.com>
2023-12-05 19:48:44 -0500

    Create sneaker completed.

--- client/products/CreateItems.jsx
+++ client/products/CreateItems.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     Card,
     CardContent,
@@ -8,20 +8,23 @@ import {
     CardActions,
     Icon,
     Button,
+    Dialog,DialogTitle,DialogContent,DialogContentText,DialogActions,Link
 } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core';
 import products from './api-products.js'
+import { Redirect } from 'react-router-dom';
 
 
 
-const CreateItems = () => {
+const CreateItems = (props) => {
     const [values, setValues] = useState({
         link: "",
         colourway: "",
         model: "",
-        price: 0
-
-    })
+        price: 0,
+    });
+    const [open, setOpen] = useState(false);
+    const [error, setError] = useState(false);
 
     const handleChange = username => event => {
         setValues({ ...values, [username]: event.target.value })
@@ -33,42 +36,71 @@ const CreateItems = () => {
             link: values.link || undefined,
             colourway: values.colourway || undefined,
             model: values.model || undefined,
-            price: values.price || undefined
+            price: values.price || undefined,
         }
 
         products.create(sneaker).then((data) => {
-            if (data.error) {
-                console.log(data.error);
-                setValues({ ...values, error: data.error })
-
+            if (data.message == "The sneaker cannot be created!") {
+                setValues({ ...values, error: '', redirectToReferrer: true });
+                setOpen(true);
             } else {
-                setValues({ ...values, error: '', redirectToReferrer: true })
+                // useEffect (() => { setError(true) }, []);
+                console.log(error);
             }
         })
+    }
+
+    function handleClose() {
+        return <Redirect to="/"/>;
     }
 
     return (
         <Card >
             <CardContent>
                 <Typography variant="h5">
-                    Edit Sneaker
+                    Create Sneaker
                 </Typography>
 
                 <TextField id="link" label="Image Link" value={values.link} onChange={handleChange('link')} margin="normal" /><br />
                 <TextField id="colourway" label="Colourway" value={values.colourway} onChange={handleChange('colourway')} margin="normal" /><br />
                 <TextField id="model" label="Model" value={values.model} onChange={handleChange('model')} margin="normal" /><br />
                 <TextField id="price" label="Price" value={values.price} onChange={handleChange('price')} margin="normal" /><br />
-
-                <br /> {
-                    values.error && (<Typography component="p" color="error">
-                        <Icon color="error" className={classes.error}>error</Icon>
-                        {values.error}
-                    </Typography>)
+                <br />
+                {
+                    values.error &&
+                    (
+                        <Typography component="p" color="error">
+                            <Icon color="error" className={classes.error}>error</Icon>
+                            {values.error}
+                        </Typography>
+                    )
                 }
             </CardContent>
             <CardActions>
                 <Button color="primary" variant="contained" onClick={clickSubmit}>Create Sneaker</Button>
             </CardActions>
+
+
+            {/* <Dialog open={open} onClose={handleClose} disableEscapeKeyDown>
+                <DialogTitle>New Sneaker</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        New sneaker successfully created.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Link to="/featuredItems">
+                        <Button
+                            color="primary"
+                            autoFocus
+                            variant="contained"
+                            onClick={handleClose}
+                        >
+                            Return home
+                        </Button>
+                    </Link>
+                </DialogActions>
+            </Dialog> */}
         </Card>
     );
 };
